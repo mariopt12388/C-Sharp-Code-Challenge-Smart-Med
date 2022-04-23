e29c70c64110548dc791a155f5919636cbf4347f
Mario_1170565 <1170565@isep.ipp.pt>
2022-04-23 16:38:44 +0100

    Basic unit tests using XUnit.

---
+++ SmartMedCodeChallenge1/UnitTests/MedicationsUnitTests.cs
@@ -0,0 +1,69 @@
+﻿using SmartMedCodeChallenge1.Controllers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartMedCodeChallenge1.UnitTests
+{
+    public class MedicationsUnitTests
+    {
+        ValuesController valuesController = new ValuesController();
+
+        [Fact]
+        public void Get()
+        {
+            //Arrange
+            var expectedResponse = "No data found";
+            var response = valuesController.Get(-1);
+
+            
+
+            //Assert
+            Assert.Matches(expectedResponse, response);
+        }
+
+        [Fact]
+        public void Post()
+        {
+            //Arrange
+            var expectedResponse = "Record inserted with the name as " + "test" + ", the quantity as " + 1 + " and the creation date as " + "2022-12-12 12:12:12";
+            var response = valuesController.Post("test", 1, "2022-12-12 12:12:12");
+
+
+
+
+
+            //Assert
+            Assert.Matches(expectedResponse, response);
+        }
+
+        [Fact]
+        public void PUT()
+        {
+            //Arrange
+            var expectedResponse = "Record updated with the name as " + "test" + ", the quantity as " + 1 + " and the creation date as " + "2022-12-12 12:12:12" + " and the id as " + 1;
+            var response = valuesController.Put(1, "test", 1, "2022-12-12 12:12:12");
+
+
+
+
+
+            //Assert
+            Assert.Matches(expectedResponse, response);
+        }
+
+        [Fact]
+        public void DELETE()
+        {
+            //Arrange
+            var expectedResponse = "Record deleted with the  id as " + 1;
+            var response = valuesController.Delete(1);
+
+
+
+
+
+            //Assert
+            Assert.Matches(expectedResponse, response);
+        }
+    }
+}
